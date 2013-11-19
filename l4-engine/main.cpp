3628aca8d273bd236748533751f6c17cb135fa3b
unknown <george.lamperis@gmail.com>
2013-11-19 13:34:59 -0600

    first commit.

---
+++ l4-engine/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define DEBUG_LOG 1
+
+void uci_identify()
+{
+
+}
+
+
+int main ()
+{
+	string Line; //to read the command given by the GUI
+	int flag =1; //to change the value of chess squares from 'a' to 'h'
+
+	cout.setf (ios::unitbuf);// Make sure that the outputs are sent straight away to the GUI
+
+	while( getline( cin, Line ) ) {
+		if ( Line == "uci" ) {
+			cout << "id name l4-engine" << endl;
+			cout << "id author George Lamperis" << endl;
+			cout << "uciok" << endl;
+		} else if ( Line == "quit" ) {
+			cout << "Bye Bye" << endl;
+			break;
+		} else if ( Line == "isready" ) {
+			cout << "readyok" << endl;
+		} else if ( Line == "ucinewgame" ) {
+			; // nothing to do
+		}
+
+		if ( Line.substr(0,23) == "position startpos moves " ) {
+			; // nothing to do
+		} else if ( Line == "stop" ) {
+			; // nothing to do
+		} else if ( Line.substr( 0, 3 ) == "go " ) {
+			// Received a command like: "go wtime 300000 btime 300000 winc 0 binc 0"
+			cout << "bestmove " << char(105-flag) << "7" << char(105-flag) << "5" << endl;
+			//Output like: "bestmove h7h5"
+			flag++; //increase flag to move other pawn on next turn
+		}
+	}
+
+	return 0;
+}
