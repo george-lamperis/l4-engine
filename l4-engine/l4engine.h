2689e75f212ba59ce608254a8c5acbba7dd6303b
unknown <george.lamperis@gmail.com>
2013-11-21 13:10:07 -0600

    added comment about encoding.

--- l4-engine/l4engine.h
+++ l4-engine/l4engine.h
@@ -39,12 +39,43 @@ struct chessboard_t {
 };
 
 // const
-struct chessboard_t chessboard_initial_state;
+// struct chessboard_t chessboard_initial_state;
 
 // -----------------------------------------------------------------------------
 // Bitboards and lookup tables
+// lookup tables are indexed left to right, top to bottom
 // -----------------------------------------------------------------------------
 
+// LERF encoding
+// inspired/copied from
+// http://pages.cs.wisc.edu/~psilord/blog/data/chess-pages/rep.html
+// http://chessprogramming.wikispaces.com/Square+Mapping+Considerations
+//
+//        a    b    c    d    e    f    g    h
+//      +----+----+----+----+----+----+----+----+
+//    8 | 56 | 57 | 58 | 59 | 60 | 61 | 62 | 63 | 8
+//      +----+----+----+----+----+----+----+----+
+//    7 | 48 | 49 | 50 | 51 | 52 | 53 | 54 | 55 | 7
+//      +----+----+----+----+----+----+----+----+
+//    6 | 40 | 41 | 42 | 43 | 44 | 45 | 46 | 47 | 6
+//      +----+----+----+----+----+----+----+----+
+//    5 | 32 | 33 | 34 | 35 | 36 | 37 | 38 | 39 | 5
+//      +----+----+----+----+----+----+----+----+
+//    4 | 24 | 25 | 26 | 27 | 28 | 29 | 30 | 31 | 4
+//      +----+----+----+----+----+----+----+----+
+//    3 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 3
+//      +----+----+----+----+----+----+----+----+
+//    2 | 8  | 9  | 10 | 11 | 12 | 13 | 14 | 15 | 2
+//      +----+----+----+----+----+----+----+----+
+//    1 | 0  | 1  | 2  | 3  | 4  | 5  | 6  | 7  | 1
+//      +----+----+----+----+----+----+----+----+
+//        a    b    c    d    e    f    g    h
 
+
+// ANDing with these results in a bitboard
+// with only the bits in that rank/file set.
+extern const bitboard_t Rank[8];
+extern const bitboard_t File[8];
+extern const bitboard_t Square[8];
 
 #endif // L4_ENGINE_H
