335527b42b3d3255929b157ba2d687879b043ae9
unknown <george.lamperis@gmail.com>
2013-11-20 21:09:20 -0600

    set up unit testing project.

---
+++ tests/test1.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+
+TEST(FactorialTest, HandlesZeroInput) {
+  EXPECT_EQ(1, 0);
+}
+
+TEST(sample_test_case, sample_test)
+{
+    EXPECT_EQ(1, 1);
+}
+
+int main(int argc, char** argv) 
+{ 
+    testing::InitGoogleTest(&argc, argv); 
+    RUN_ALL_TESTS(); 
+    std::getchar(); // keep console window open until Return keystroke
+}
