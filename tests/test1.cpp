4bbac34314819e77f139fc2d10f8ff7f6b9e0f5a
unknown <george.lamperis@gmail.com>
2013-12-02 19:31:23 -0600

    tweaked project setup for tests.

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,11 +1,18 @@
 #include <gtest/gtest.h>
 
+#include "l4engine.h"
+
 TEST(FactorialTest, HandlesZeroInput) {
   EXPECT_EQ(1, 0);
 }
 
 TEST(sample_test_case, sample_test)
 {
+    EXPECT_EQ(1, 1);
+
+    bitboard_t x = file_mask(FILE_A);
+    print_bitboard(x);
+
     EXPECT_EQ(1, 1);
 }
 
