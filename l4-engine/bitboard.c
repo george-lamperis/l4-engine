42fae05fc32b8711660ad79d0eab70276ee6659a
George Lamperis <george.lamperis@gmail.com>
2013-12-09 15:42:01 -0600

    moved bit_count function.

--- l4-engine/bitboard.c
+++ l4-engine/bitboard.c
@@ -57,6 +57,22 @@ bitboard_t square_mask(enum eSquare sq)
 }
 
 
+// Brian Kernighan's way
+// Consecutively reset LS1B in a loop body and counting loop cycles until the
+// bitset becomes empty. Brian Kernighan mentioned the trick in his and
+// Ritchie's book The C Programming_Language, 2nd Edition 1988, exercise 2-9.
+int bit_count (bitboard_t x)
+{
+    int count = 0;
+    while (x) {
+        count++;
+        x &= x - 1; // reset LS1B
+    }
+
+    return count;
+}
+
+
 void print_bitboard(bitboard_t b) 
 {
     printf("\t    a   b   c   d   e   f   g   h  \n");
