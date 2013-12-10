234fff9d6a80ffe627f169858721483240f2b6a7
unknown <george.lamperis@gmail.com>
2013-12-10 03:27:08 -0600

    project compiles on MSVC 2013 now.

--- l4-engine/uci.c
+++ l4-engine/uci.c
@@ -26,35 +26,7 @@ void uci_readyok()
 
 void uci_input()
 {
-    string Line; //to read the command given by the GUI
-	int flag =1; //to change the value of chess squares from 'a' to 'h'
-
-	cout.setf (ios::unitbuf);// Make sure that the outputs are sent straight away to the GUI
-
-	while( getline( cin, Line ) ) {
-		if ( Line == "uci" ) {
-            uci_identify();
-		} else if ( Line == "quit" ) {
-			cout << "Bye Bye" << endl;
-			break;
-		} else if ( Line == "isready" ) {
-            uci_readyok();
-		} else if ( Line == "ucinewgame" ) {
-			; // nothing to do
-		}
-
-		if ( Line.substr(0,23) == "position startpos moves " ) {
-			; // nothing to do
-		} else if ( Line == "stop" ) {
-			; // nothing to do
-		} else if ( Line.substr( 0, 3 ) == "go " ) {
-			// Received a command like: "go wtime 300000 btime 300000 winc 0 binc 0"
-			cout << "bestmove " << char(105-flag) << "7" << char(105-flag) << "5" << endl;
-			//Output like: "bestmove h7h5"
-			flag++; //increase flag to move other pawn on next turn
-		}
-	}
-
+    char buffer[256];
 }
 
 
@@ -81,5 +53,6 @@ struct chessboard_t parse_fen(const char *fen)
         }
     }
 
-    return chess_initial_state;
+    struct chessboard_t x = {0};
+    return x;
 }
